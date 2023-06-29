98912a13f5d28f1910bcabbfdfd61c1f07c1aa37
Nash Aguayo <nash.aguayo@gmail.com>
2023-06-29 17:39:18 -0300

    Added missing axios setup

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,22 @@
   </div>
 </template>
 
+<script>
+import axios from './config/axios';
+
+export default {
+  name: 'App',
+  async created() {
+    const response = await axios.get('pokemon/1');
+    if (response.status === 200) {
+      console.log('Ok');
+    } else {
+      console.error('Something went wrong. Unable to connect to axios!');
+    }
+  },
+};
+</script>
+
 <style>
 #app {
 }
