56047306f5cb97fc50f2b5aca035acd058bd57ae
Nash Aguayo <nash.aguayo@gmail.com>
2023-06-29 17:10:09 -0300

    Installed vue router

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <template>
   <div id="app">
-    <h1>Pokedex</h1>
-    <p>This is a website about pokemons.</p>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
-
-<script>
-import axios from './config/axios';
-
-export default {
-  name: 'App',
-  async created() {
-    const response = await axios.get('pokemon/1');
-    if (response.status === 200) {
-      console.log('Ok');
-    } else {
-      console.error('Something went wrong. Unable to connect to axios!');
-    }
-  },
-};
-</script>
 
 <style>
 #app {
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Pokedex</h1>
+    <span>This is an app about Pokemons.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>
