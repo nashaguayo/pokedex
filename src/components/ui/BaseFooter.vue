bedc1fd669ee3a6fdb4220f53224f64eb1c98364
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-01 14:02:23 -0300

    Added a link to pokeapi in footer

--- src/components/ui/BaseFooter.vue
+++ src/components/ui/BaseFooter.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="base-footer">
     <div id="disclaimer">
-      <span>This is a project made with love for learning.</span><br />
-      <span>Please checkout the github repo and send your thoughts.</span><br />
-      <span>Thank you for sharing!</span>
+      <span>This is a project built with love, for learning.</span><br />
+      <span>Please checkout the github repo and send your thoughts.</span>
     </div>
+    <CenteredColumn id="fueled-by">
+      <span>Fueled by</span>
+      <a href="https://pokeapi.co/docs/v2" target="_blank" ref="noreferrer">
+        <img src="@assets/ui/pokeapi.png" alt="pokapi logo" />
+      </a>
+    </CenteredColumn>
     <a :href="githubRepoUrl" target="_blank" ref="noreferrer">
       <img
         src="@assets/ui/github-logo.jpeg"
@@ -16,8 +21,11 @@
 </template>
 
 <script>
+import CenteredColumn from './CenteredColumn.vue';
+
 export default {
   name: 'BaseFooter',
+  components: { CenteredColumn },
   data() {
     return {
       githubRepoUrl: process.env.VUE_APP_GITHUB_REPO_URL,
@@ -41,10 +49,27 @@ export default {
   justify-content: space-between;
 
   #disclaimer {
-    margin: 1rem;
+    margin: 0 1rem;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: center;
 
     span {
       color: $variant-text-color;
+      font-size: 1rem;
+    }
+  }
+
+  #fueled-by {
+    justify-content: center;
+
+    span {
+      color: $variant-text-color;
+    }
+
+    img {
+      width: 10rem;
     }
   }
 
