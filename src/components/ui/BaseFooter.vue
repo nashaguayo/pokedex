f566a77878b81997a6cb0ccf118de0883a3b9cf1
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-01 11:27:21 -0300

    Added disclaimer and github logo

--- src/components/ui/BaseFooter.vue
+++ src/components/ui/BaseFooter.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="base-footer">
-    <h1>This is a footer</h1>
+    <div id="disclaimer">
+      <span>This is a project made with love for learning.</span><br />
+      <span>Please checkout the github repo and send your thoughts.</span><br />
+      <span>Thank you for sharing!</span>
+    </div>
+    <img
+      src="@assets/ui/github-logo.jpeg"
+      class="github-logo"
+      alt="github logo"
+    />
   </div>
 </template>
 
@@ -20,5 +29,20 @@ export default {
   height: 7rem;
   bottom: 0;
   margin-top: auto;
+  display: flex;
+  justify-content: space-between;
+
+  #disclaimer {
+    margin: 1rem;
+
+    span {
+      color: $variant-text-color;
+    }
+  }
+
+  .github-logo {
+    width: 4.2rem;
+    margin: 1rem;
+  }
 }
 </style>
