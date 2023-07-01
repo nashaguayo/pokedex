c5e98c1793960ab36618d59e77c62dd58f8b4ca9
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-01 11:11:42 -0300

    Created BaseFooter component

---
+++ src/components/ui/BaseFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="base-footer">
+    <h1>This is a footer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseFooter',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@css/colors.scss';
+
+.base-footer {
+  position: relative;
+  background-color: $variant-background-color;
+  width: 100%;
+  height: 7rem;
+  bottom: 0;
+  margin-top: auto;
+}
+</style>
