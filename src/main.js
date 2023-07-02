d060427438de056f76e33776d28da78e9db15875
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-02 13:22:10 -0300

    Button has margin bottom only when footer is visible

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import { library } from '@fortawesome/fontawesome-svg-core';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faToggleOn } from '@fortawesome/free-solid-svg-icons';
 import { faToggleOff } from '@fortawesome/free-solid-svg-icons';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+import VueObserveVisibility from 'vue-observe-visibility';
 
 library.add(faToggleOn);
 library.add(faToggleOff);
 library.add(faArrowUp);
 
 Vue.component('FontAwesomeIcon', FontAwesomeIcon);
+
+Vue.use(VueObserveVisibility);
 
 Vue.config.productionTip = false;
 
