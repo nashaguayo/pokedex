b93b683533ae465bb00f7fa489ddbc44b894bcdb
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-04 15:23:22 -0300

    Extracted component

--- src/components/pokemons/PokemonItem.vue
+++ src/components/pokemons/PokemonItem.vue
@@ -1,20 +1,9 @@
 <template>
   <CenteredColumn class="pokemon-item" ref="pokemonItem">
-    <img
-      class="location"
-      src="@assets/pokemons/location.jpg"
-      :height="locationHeight"
-      :width="locationWidth"
+    <PokemonItemHeader
+      :pokemonName="getCapitalizedPokemonName()"
+      :pokemonImage="pokemonImage"
     />
-    <img
-      class="pokemon-image"
-      :src="pokemonImage"
-      alt="pokemon"
-      ref="pokemonImage"
-      @load="setLocationHeight"
-    />
-    <div class="pokemon-backdrop-filter"></div>
-    <h2 class="pokemon-name">{{ getCapitalizedPokemonName() }}</h2>
     <div class="pokemon-info-container">
       <BaseButton :onClickHandler="goBack">Go Back</BaseButton>
     </div>
@@ -24,6 +13,7 @@
 <script>
 import BaseButton from '@components/ui/BaseButton.vue';
 import CenteredColumn from '@components/ui/CenteredColumn.vue';
+import PokemonItemHeader from '@components/pokemons/PokemonItemHeader.vue';
 import { getPokemon } from '@api/pokemon';
 
 export default {
@@ -31,13 +21,12 @@ export default {
   components: {
     BaseButton,
     CenteredColumn,
+    PokemonItemHeader,
   },
   data() {
     return {
       pokemon: {},
       pokemonImage: '',
-      locationHeight: 0,
-      locationWidth: 0,
     };
   },
   async created() {
@@ -58,10 +47,6 @@ export default {
       document.title = `Pokedex - ${pokemonNameCapitalized}`;
       return pokemonNameCapitalized;
     },
-    setLocationHeight() {
-      this.locationHeight = this.$refs.pokemonImage.offsetHeight;
-      this.locationWidth = this.$refs.pokemonItem.offsetWidth;
-    },
   },
 };
 </script>
@@ -70,61 +55,6 @@ export default {
 @import '@css/media-queries.scss';
 
 .pokemon-item {
-  .location {
-    position: absolute;
-
-    @media (min-width: $min-width-second-break) {
-      width: 75%;
-    }
-  }
-  .pokemon-image {
-    width: 20rem;
-    z-index: 5;
-    padding: 2rem 0;
-
-    @media (min-width: $min-width-first-break) {
-      width: 25rem;
-    }
-
-    @media (min-width: $min-width-second-break) {
-      width: 30rem;
-    }
-  }
-
-  .pokemon-backdrop-filter {
-    position: relative;
-    height: 2.5rem;
-    width: 100%;
-    z-index: 5;
-    top: -5rem;
-    backdrop-filter: blur(1rem);
-
-    @media (min-width: $min-width-first-break) {
-      top: -7rem;
-      height: 4rem;
-    }
-
-    @media (min-width: $min-width-second-break) {
-      top: -9rem;
-      height: 5rem;
-    }
-  }
-
-  .pokemon-name {
-    position: relative;
-    top: -8rem;
-    z-index: 10;
-    color: var(--variant-text-color) !important;
-
-    @media (min-width: $min-width-first-break) {
-      top: -11rem;
-    }
-
-    @media (min-width: $min-width-second-break) {
-      top: -14.5rem;
-    }
-  }
-
   .pokemon-info-container {
     position: relative;
     top: -5rem;
---
+++ src/components/pokemons/PokemonItemHeader.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="pokemon-item-header">
+    <img
+      class="location"
+      src="@assets/pokemons/location.jpg"
+      :height="locationHeight"
+      :width="locationWidth"
+    />
+    <img
+      class="pokemon-image"
+      :src="pokemonImage"
+      alt="pokemon"
+      ref="pokemonImage"
+      @load="setLocationHeight"
+    />
+    <div class="pokemon-backdrop-filter"></div>
+    <h2 class="pokemon-name">{{ pokemonName }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokemonItemHeader',
+  data() {
+    return {
+      locationHeight: 0,
+      locationWidth: 0,
+    };
+  },
+  props: {
+    pokemonImage: {
+      type: String,
+      required: true,
+    },
+    pokemonName: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    setLocationHeight() {
+      this.locationHeight = this.$refs.pokemonImage.offsetHeight;
+      this.locationWidth = this.$refs.pokemonItem.offsetWidth;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@css/media-queries.scss';
+
+.pokemon-item-header {
+  .location {
+    position: absolute;
+
+    @media (min-width: $min-width-second-break) {
+      width: 75%;
+    }
+  }
+
+  .pokemon-backdrop-filter {
+    position: relative;
+    height: 2.5rem;
+    width: 100%;
+    z-index: 5;
+    top: -5rem;
+    backdrop-filter: blur(1rem);
+
+    @media (min-width: $min-width-first-break) {
+      top: -7rem;
+      height: 4rem;
+    }
+
+    @media (min-width: $min-width-second-break) {
+      top: -9rem;
+      height: 5rem;
+    }
+  }
+
+  .pokemon-image {
+    width: 20rem;
+    z-index: 5;
+    padding: 2rem 0;
+
+    @media (min-width: $min-width-first-break) {
+      width: 25rem;
+    }
+
+    @media (min-width: $min-width-second-break) {
+      width: 30rem;
+    }
+  }
+
+  .pokemon-name {
+    position: relative;
+    top: -8rem;
+    z-index: 10;
+    color: var(--variant-text-color) !important;
+
+    @media (min-width: $min-width-first-break) {
+      top: -11rem;
+    }
+
+    @media (min-width: $min-width-second-break) {
+      top: -14.5rem;
+    }
+  }
+}
+</style>
