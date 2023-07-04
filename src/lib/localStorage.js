60f9a20f984c2541dd1510390884841b1f304caf
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-03 22:19:58 -0300

    Fixed bug

--- src/lib/localStorage.js
+++ src/lib/localStorage.js
@@ -1,10 +1,10 @@
 const DARKMODE_ENABLED = 'darkmodeEnabled';
 
 export function isDarkModeEnabled() {
-  return localStorage.getItem(DARKMODE_ENABLED) === 'true' ?? false;
+  return localStorage.getItem(DARKMODE_ENABLED) === 'true';
 }
 
 export function toggleDarkMode() {
-  const isDarkModeEnabled = isDarkModeEnabled();
+  const isDarkModeEnabled = localStorage.getItem(DARKMODE_ENABLED) === 'true';
   localStorage.setItem(DARKMODE_ENABLED, !isDarkModeEnabled);
 }
