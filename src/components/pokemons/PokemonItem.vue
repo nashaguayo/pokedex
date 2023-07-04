9814f7caf36c7c754eb763c82118d069467c3367
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-04 18:47:10 -0300

    fixed warning

--- src/components/pokemons/PokemonItem.vue
+++ src/components/pokemons/PokemonItem.vue
@@ -87,7 +87,7 @@ export default {
     display: flex;
     flex-direction: row-reverse;
     justify-content: space-evenly;
-    align-items: start;
+    align-items: flex-start;
     margin-top: 3rem;
   }
 
