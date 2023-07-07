211a40ff21081a893e2fbd0ef600cf0d471411dc
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-06 21:06:12 -0300

    Created Error Boundary component and using it in pokemon details page

---
+++ src/components/ui/ErrorBoundary.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'ErrorBoundary',
+  data: () => ({
+    error: false,
+  }),
+  props: {
+    componentName: {
+      type: String,
+      required: true,
+    },
+    errorMessage: {
+      type: String,
+      required: true,
+    },
+  },
+  errorCaptured() {
+    this.error = true;
+  },
+  render(h) {
+    return this.error
+      ? h('p', `Something went wrong! ${this.errorMessage}`)
+      : this.$slots.default[0];
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  text-align: center;
+}
+</style>
