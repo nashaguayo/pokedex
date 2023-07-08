a473c73acb7050309a8cb75d9216f23184af94f3
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-08 20:40:26 -0300

    Added error boundary

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,16 +1,22 @@
 <template>
   <CenteredColumn>
-    <PokemonSearch />
+    <ErrorBoundary
+      componentName="PokemonSearch"
+      errorMessage="Unable to load search"
+    >
+      <PokemonSearch />
+    </ErrorBoundary>
   </CenteredColumn>
 </template>
 
 <script>
 import CenteredColumn from '@components/ui/CenteredColumn';
+import ErrorBoundary from '@components/ui/ErrorBoundary.vue';
 import PokemonSearch from '@components/search/PokemonSearch';
 
 export default {
   name: 'SearchView',
   title: 'Search for Pokemons',
-  components: { CenteredColumn, PokemonSearch },
+  components: { CenteredColumn, ErrorBoundary, PokemonSearch },
 };
 </script>
