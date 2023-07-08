c2d8e4922d1cb490226f006857d7232cf990fa7b
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-08 11:06:18 -0300

    Fixed failing unit test

--- tests/unit/views/HomeView.test.js
+++ tests/unit/views/HomeView.test.js
@@ -1,9 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
 import HomeView from '@views/HomeView.vue';
 
-jest.mock('@components/ui/BasePage.vue', () => ({
-  name: 'BasePage',
-  template: '<div class="mocked-base-page"></div>',
+jest.mock('@components/ui/CenteredColumn.vue', () => ({
+  name: 'CenteredColumn',
+  template: '<div class="mocked-centered-column"></div>',
 }));
 
 jest.mock('@components/home/LogoAndBanner.vue', () => ({
@@ -19,7 +19,7 @@ jest.mock('@components/home/RandomPokemon.vue', () => ({
 describe('HomeView', () => {
   it('renders all components components', () => {
     const wrapper = shallowMount(HomeView);
-    expect(wrapper.find('basepage-stub').exists()).toBe(true);
+    expect(wrapper.find('centeredcolumn-stub').exists()).toBe(true);
     expect(wrapper.find('logoandbanner-stub').exists()).toBe(true);
     expect(wrapper.find('randompokemon-stub').exists()).toBe(true);
   });
