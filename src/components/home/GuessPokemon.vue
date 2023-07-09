76e637165c77802eccaf5dff8158c479d6af3568
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-08 21:09:15 -0300

    Added get new pokemon functionality

--- src/components/home/GuessPokemon.vue
+++ src/components/home/GuessPokemon.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="guess-pokemon">
+  <CenteredColumn class="guess-pokemon">
     <h2>Guess the Pokemon</h2>
     <div class="background-image">
       <img :src="image" alt="mysterious pokemon" />
     </div>
-  </div>
+    <BaseButton :big="true" :onClickHandler="getNewMysteryPokemon">
+      Guess New Pokemon
+    </BaseButton>
+  </CenteredColumn>
 </template>
 
 <script>
+import CenteredColumn from '@components/ui/CenteredColumn';
+import BaseButton from '@components/ui/BaseButton';
 import store from '@lib/store';
 
 export default {
   name: 'GuessPokemon',
+  components: { CenteredColumn, BaseButton },
   computed: {
     image() {
       return store.state.game.image;
@@ -21,7 +27,12 @@ export default {
     },
   },
   created() {
-    store.getNewMysteryPokemon();
+    this.getNewMysteryPokemon();
+  },
+  methods: {
+    getNewMysteryPokemon() {
+      store.getNewMysteryPokemon();
+    },
   },
 };
 </script>
@@ -29,6 +40,8 @@ export default {
 <style lang="scss" scoped>
 .guess-pokemon {
   margin-bottom: 2rem;
+
+  max-width: 25rem;
   .background-image {
     background-image: url(@assets/home/mystery-pokemon.jpg);
     background-size: cover;
@@ -37,6 +50,7 @@ export default {
     border: 0.2rem solid var(--main-border-color);
     box-shadow: var(--main-box-shadow);
     border-radius: 1rem;
+    margin-bottom: 2rem;
 
     img {
       margin: 1rem;
