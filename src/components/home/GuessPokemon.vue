1b58d2c2a96e2be1c04f980271421c4a0bb26f51
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-08 21:01:29 -0300

    Showing image of random pokemon

--- src/components/home/GuessPokemon.vue
+++ src/components/home/GuessPokemon.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="guess-pokemon">
     <h2>Guess the Pokemon</h2>
-    <div class="background-image"></div>
+    <div class="background-image">
+      <img :src="image" alt="mysterious pokemon" />
+    </div>
   </div>
 </template>
 
 <script>
+import store from '@lib/store';
+
 export default {
   name: 'GuessPokemon',
+  computed: {
+    image() {
+      return store.state.game.image;
+    },
+    name() {
+      return store.state.game.name;
+    },
+  },
+  created() {
+    store.getNewMysteryPokemon();
+  },
 };
 </script>
 
@@ -22,6 +37,12 @@ export default {
     border: 0.2rem solid var(--main-border-color);
     box-shadow: var(--main-box-shadow);
     border-radius: 1rem;
+
+    img {
+      margin: 1rem;
+      filter: brightness(0);
+      width: 10rem;
+    }
   }
 }
 </style>
