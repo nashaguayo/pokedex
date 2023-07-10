a8aaab603d4a76b7733c1c91f3f69c993755ffd3
nashaguayo <77163978+nashaguayo@users.noreply.github.com>
2023-07-10 20:19:33 -0300

    [PD-67] Guess Pokemon: limiting guesses and giving awards (#84)
    * FEAT: Adding loader to image
    
    * FEAT: Added transitions
    
    * FIX: Added timer to get new pokemon in a row
    
    * CHORE: Removed console.log
    
    * CHORE: Cleaned up code
    
    * FEAT: Showing bronze, metal, and gold stars
    
    * FIX: When guessing on last try, it not longer says you lost
    
    * FEAT: Autofocusing input
    
    * FIX: Keyboard doesn't display on loading home
    
    * TEST: Added unit tests

--- src/components/home/GuessPokemon.vue
+++ src/components/home/GuessPokemon.vue
@@ -1,17 +1,22 @@
 <template>
   <CenteredColumn class="guess-pokemon">
     <div class="background-image">
-      <img
-        :src="image"
-        alt="mysterious pokemon"
-        :class="{ 'is-guessing': !hasWon, 'show-pokemon': hasWon || hasLost }"
-      />
+      <BaseLoader :loading="loading">
+        <img
+          :src="image"
+          alt="mysterious pokemon"
+          :class="{ 'is-guessing': !hasWon, 'show-pokemon': hasWon || hasLost }"
+        />
+      </BaseLoader>
     </div>
-    <span
-      id="game-results"
-      :class="{ losing: !hasWon && playersGuess.length, winning: hasWon }"
-      >{{ gameResultsText }}</span
-    >
+    <transition name="flip" mode="out-in">
+      <span
+        id="game-results"
+        :key="gameResultsText"
+        :class="{ losing: !hasWon && playersGuess.length, winning: hasWon }"
+        >{{ gameResultsText }}</span
+      >
+    </transition>
     <BaseInput
       name="guess"
       placeholder="Insert pokemon name here..."
@@ -20,26 +25,59 @@
       :reset="reset"
       :lazy="true"
       :disabled="hasLost || hasWon"
+      :focus="focus"
+      @focused="focus = false"
     />
-    <span v-if="!hasWon" class="tries-left">{{ triesLeftText }}</span>
-    <CenteredColumn v-if="guessesInARow > 0" class="guesses-in-a-row">
-      <span>Guesses in a row</span><br />
-      <div class="stars">
-        <FontAwesomeIcon
-          v-for="guess in guessesInARow"
-          :key="`guess-${guess}`"
-          icon="fa-solid fa-star"
-          class="star"
-        />
-      </div>
-    </CenteredColumn>
-    <BaseButton :big="true" :onClickHandler="getNewMysteryPokemon">
+    <transition name="flip" mode="out-in">
+      <span :key="triesLeftText" class="tries-left">{{ triesLeftText }}</span>
+    </transition>
+    <transition name="flip" appear>
+      <CenteredColumn v-if="guessesInARow > 0" class="guesses-in-a-row">
+        <span>Guesses in a row</span><br />
+        <div class="stars">
+          <transition-group name="zoom-in" appear>
+            <FontAwesomeIcon
+              v-for="guess in goldStars"
+              :key="`guess-${guess}`"
+              icon="fa-solid fa-star"
+              class="gold-star"
+            />
+          </transition-group>
+        </div>
+        <div class="stars">
+          <transition-group name="zoom-in" appear>
+            <FontAwesomeIcon
+              v-for="guess in silverStars"
+              :key="`guess-${guess}`"
+              icon="fa-solid fa-star"
+              class="silver-star"
+            />
+          </transition-group>
+        </div>
+        <div class="stars">
+          <transition-group name="zoom-in" appear>
+            <FontAwesomeIcon
+              v-for="guess in bronzeStars"
+              :key="`guess-${guess}`"
+              icon="fa-solid fa-star"
+              class="bronze-star"
+            />
+          </transition-group>
+        </div>
+      </CenteredColumn>
+    </transition>
+    <BaseButton
+      :big="true"
+      :onClickHandler="getNewMysteryPokemonAndRefreshGuessesInARow"
+      :disabled="hasWon"
+    >
       {{ baseButtonText }}
     </BaseButton>
   </CenteredColumn>
 </template>
 
 <script>
+import BaseLoader from '@components/ui/BaseLoader';
 import CenteredColumn from '@components/ui/CenteredColumn';
 import BaseInput from '@components/ui/BaseInput';
 import BaseButton from '@components/ui/BaseButton';
@@ -47,13 +85,22 @@ import store from '@lib/store';
 
 export default {
   name: 'GuessPokemon',
-  components: { CenteredColumn, BaseButton, BaseInput },
+  components: { BaseLoader, CenteredColumn, BaseButton, BaseInput },
   data() {
     return {
       playersGuess: '',
       reset: false,
       tries: 3,
       guessesInARow: 0,
+      loading: false,
+      timerCount: 5,
+      timerEnabled: false,
+      bronzeStars: 0,
+      silverStars: 0,
+      goldStars: 0,
+      platinumStars: 0,
+      focus: false,
+      isFirstTime: true,
     };
   },
   computed: {
@@ -64,7 +111,9 @@ export default {
       return store.state.game.name;
     },
     triesLeftText() {
-      return this.hasLost
+      return this.hasWon
+        ? `Getting new Pokemon in ${this.timerCount}...`
+        : this.hasLost
         ? `Pokemon was ${this.name}`
         : `You have ${this.tries} ${this.tries === 1 ? 'try' : 'tries'} left`;
     },
@@ -81,7 +130,7 @@ export default {
       return this.playersGuess === this.name;
     },
     hasLost() {
-      return this.tries === 0;
+      return !this.hasWon && this.tries === 0;
     },
     baseButtonText() {
       return this.hasWon || this.hasLost ? 'New Pokemon!' : 'I Give Up!';
@@ -90,6 +139,7 @@ export default {
   watch: {
     hasWon(hasWon) {
       if (hasWon) {
+        this.timerEnabled = true;
         this.guessesInARow++;
       }
     },
@@ -98,21 +148,71 @@ export default {
         this.guessesInARow = 0;
       }
     },
+    timerEnabled(enabled) {
+      if (enabled) {
+        setTimeout(() => {
+          this.timerCount--;
+        }, 1000);
+      } else {
+        this.timerCount = 5;
+      }
+    },
+    timerCount(count) {
+      if (count > 0 && this.timerEnabled) {
+        setTimeout(() => {
+          this.timerCount--;
+        }, 1000);
+        return;
+      } else if (count === 0) {
+        this.timerEnabled = false;
+        this.getNewMysteryPokemon();
+      }
+    },
+    guessesInARow(guesses) {
+      if (guesses < 5) {
+        this.bronzeStars = guesses;
+        return;
+      }
+
+      if (guesses < 25) {
+        this.silverStars = Math.floor(guesses / 5);
+        this.bronzeStars = guesses % 5;
+        return;
+      }
+
+      this.goldStars = Math.floor(guesses / 25);
+      this.silverStars = Math.floor((guesses % 25) / 5);
+      this.bronzeStars = (guesses % 25) % 5;
+    },
   },
   async created() {
-    await this.getNewMysteryPokemon();
+    await this.getNewMysteryPokemon('created');
   },
   methods: {
     async getNewMysteryPokemon() {
+      this.loading = true;
       await store.getNewMysteryPokemon();
       this.reset = true;
       this.playersGuess = '';
       this.tries = 3;
+      this.loading = false;
+
+      if (!this.isFirstTime) {
+        this.focus = true;
+      } else {
+        this.isFirstTime = false;
+      }
     },
     setPlayersGuess(playersGuess) {
-      this.tries--;
       this.reset = false;
       this.playersGuess = playersGuess;
+      if (!this.hasWon) {
+        this.tries--;
+      }
+    },
+    getNewMysteryPokemonAndRefreshGuessesInARow() {
+      this.guessesInARow = 0;
+      this.getNewMysteryPokemon();
     },
   },
 };
@@ -167,11 +267,39 @@ export default {
 
   .guesses-in-a-row {
     margin-bottom: 1rem;
-
-    .star {
+    .gold-star {
       color: #edb200;
       margin: 0 0.1rem;
     }
+    .silver-star {
+      color: #b5b4b0;
+      margin: 0 0.1rem;
+    }
+    .bronze-star {
+      color: #73440f;
+      margin: 0 0.1rem;
+    }
   }
+}
+
+.flip-enter-active,
+.flip-leave-active {
+  transition: transform 0.3s;
+}
+
+.flip-enter,
+.flip-leave-to {
+  transform: scaleY(0);
+}
+
+.zoom-in-enter-active,
+.zoom-in-leave-active,
+.zoom-in-move {
+  transition: transform 0.3s;
+}
+
+.zoom-in-enter,
+.zoom-in-leave-to {
+  transform: scale(0);
 }
 </style>
