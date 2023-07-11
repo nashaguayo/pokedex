45a92d25680e0e1f2315dd017337b2bdeca7144f
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-11 19:42:43 -0300

    TEST: Added missing unit tests

--- tests/unit/components/search/PokemonSearch.test.js
+++ tests/unit/components/search/PokemonSearch.test.js
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
 import PokemonSearch from '@components/search/PokemonSearch';
 import CenteredColumn from '@components/ui/CenteredColumn';
+import store from '@lib/store';
 
 jest.mock('@components/ui/BaseInput.vue', () => ({
   name: 'BaseInput',
@@ -25,6 +26,8 @@ describe('PokemonSearch', () => {
   beforeEach(() => {
     jest.clearAllMocks();
 
+    store.clearSearchResults = jest.fn();
+
     wrapper = shallowMount(PokemonSearch, {
       components: {
         CenteredColumn,
@@ -45,5 +48,36 @@ describe('PokemonSearch', () => {
   it('displays search results', async () => {
     const resultElements = wrapper.findAll('span');
     expect(resultElements).toHaveLength(2);
+  });
+
+  it('sets the search term when input value changes', async () => {
+    wrapper.vm.searchTerm = 'pikachu';
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.searchTerm).toBe('pikachu');
+  });
+
+  it('clears search results when search term length is less than 3', async () => {
+    wrapper.setData({ searchTerm: 'pi' });
+    await wrapper.vm.$nextTick();
+    expect(store.clearSearchResults).toHaveBeenCalled();
+  });
+
+  it('searches pokemons when search term length is greater than or equal to 3', async () => {
+    wrapper.setData({ searchTerm: 'pikachu' });
+    await wrapper.vm.$nextTick();
+    expect(store.searchPokemons).toHaveBeenCalledWith('pikachu');
+  });
+
+  it('navigates to pokemon page when a search result is clicked', async () => {
+    const pokemon = 'Pikachu';
+    const mockRouter = { push: jest.fn() };
+    wrapper.vm.$router = mockRouter;
+
+    await wrapper.find('.search-result').trigger('click');
+
+    expect(mockRouter.push).toHaveBeenCalledWith({
+      name: 'pokemon',
+      params: { id: pokemon },
+    });
   });
 });
