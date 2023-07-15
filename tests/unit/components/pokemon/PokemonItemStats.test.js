8efb4e13c837215bbd4759f4406e705d6228817d
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-15 13:06:06 -0300

    TEST: Added missing unit test

--- tests/unit/components/pokemon/PokemonItemStats.test.js
+++ tests/unit/components/pokemon/PokemonItemStats.test.js
@@ -14,6 +14,10 @@ describe('PokemonItemStats', () => {
         name: 'attack',
         value: 20,
       },
+      {
+        name: 'speed',
+        value: 112,
+      },
     ];
 
     wrapper = mount(PokemonItemStats, {
@@ -30,20 +34,30 @@ describe('PokemonItemStats', () => {
 
   it('renders the correct number of stat items', () => {
     const statItems = wrapper.findAll('.pokemon-item-stat');
-    expect(statItems.length).toBe(2);
+    expect(statItems.length).toBe(3);
   });
 
   it('displays the correct stat names and values', () => {
     const statNames = wrapper.findAll('.property-names');
     const statValues = wrapper.findAll('.property-values');
 
-    expect(statNames.length).toBe(2);
-    expect(statValues.length).toBe(2);
+    expect(statNames.length).toBe(3);
+    expect(statValues.length).toBe(3);
   });
 
   it('displays the title', () => {
     const title = wrapper.find('.title');
     expect(title.exists()).toBe(true);
     expect(title.text()).toBe('Stats');
+  });
+
+  it('displays the correct amount of bars', () => {
+    const filledBars = wrapper.findAll('.filled');
+    const emptyBars = wrapper.findAll('.empty');
+    const exceededBars = wrapper.findAll('.exceeded');
+
+    expect(filledBars.length).toBe(17);
+    expect(emptyBars.length).toBe(13);
+    expect(exceededBars.length).toBe(1);
   });
 });
