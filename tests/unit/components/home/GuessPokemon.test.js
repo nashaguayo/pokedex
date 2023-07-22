4e334f8738227167ccfb68506d5854d07c0a1ce6
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-21 21:26:34 -0300

    TEST: Added missing unit tests

--- tests/unit/components/home/GuessPokemon.test.js
+++ tests/unit/components/home/GuessPokemon.test.js
@@ -37,4 +37,44 @@ describe('GuessPokemon', () => {
   it('computes the image property correctly', () => {
     expect(wrapper.vm.image).toBe('pikachu.png');
   });
+
+  it('should win the game when the correct Pokémon name is guessed', () => {
+    wrapper.vm.setPlayersGuess('pikachu');
+    expect(wrapper.vm.gameResultsText).toBe('You won!');
+    expect(wrapper.vm.hasWon).toBe(true);
+  });
+
+  it('should lose the game when running out of tries', () => {
+    wrapper.vm.setPlayersGuess('charmander');
+    expect(wrapper.vm.gameResultsText).toBe("That's not it...");
+    wrapper.vm.setPlayersGuess('bulbasaur');
+    expect(wrapper.vm.gameResultsText).toBe("That's not it...");
+    wrapper.vm.setPlayersGuess('squirtle');
+    expect(wrapper.vm.gameResultsText).toBe('You Lost!');
+    expect(wrapper.vm.hasLost).toBe(true);
+  });
+
+  it("should tell you how many tries you've got left", () => {
+    expect(wrapper.vm.triesLeftText).toBe('You have 3 tries left');
+    wrapper.vm.setPlayersGuess('charmander');
+    expect(wrapper.vm.triesLeftText).toBe('You have 2 tries left');
+    wrapper.vm.setPlayersGuess('bulbasaur');
+    expect(wrapper.vm.triesLeftText).toBe('You have 1 try left');
+    wrapper.vm.setPlayersGuess('squirtle');
+    expect(wrapper.vm.triesLeftText).toBe('Pokemon was pikachu');
+  });
+
+  it('should change button text when winning', async () => {
+    expect(wrapper.vm.baseButtonText).toBe('I Give Up!');
+    wrapper.vm.setPlayersGuess('pikachu');
+    expect(wrapper.vm.baseButtonText).toBe('New Pokemon!');
+  });
+
+  it('should change button text when losing', async () => {
+    expect(wrapper.vm.baseButtonText).toBe('I Give Up!');
+    wrapper.vm.setPlayersGuess('charmander');
+    wrapper.vm.setPlayersGuess('bulbasaur');
+    wrapper.vm.setPlayersGuess('squirtle');
+    expect(wrapper.vm.baseButtonText).toBe('New Pokemon!');
+  });
 });
