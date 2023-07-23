84a83e24feec56ddacfa41ea56968241788c1376
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-23 18:12:57 -0300

    TEST: Fixed failing unit tests

--- tests/unit/components/pokemon/PokemonItemCharacteristics.test.js
+++ tests/unit/components/pokemon/PokemonItemCharacteristics.test.js
@@ -7,6 +7,7 @@ describe('LogoAndBanner', () => {
   beforeEach(() => {
     wrapper = shallowMount(PokemonItemCharacteristics, {
       propsData: {
+        id: 1,
         characteristic: 'Mischievous',
         weight: 80,
         height: 17,
@@ -26,29 +27,34 @@ describe('LogoAndBanner', () => {
     expect(characteristic.text()).toBe('Mischievous');
   });
 
-  it('displays the correct weight', () => {
+  it('displays the correct id', () => {
     const weight = wrapper.findAll('span').at(1);
+    expect(weight.text()).toBe('#1');
+  });
+
+  it('displays the correct weight', () => {
+    const weight = wrapper.findAll('span').at(3);
     expect(weight.text()).toBe('80 pounds');
   });
 
   it('displays the correct height', () => {
-    const height = wrapper.findAll('span').at(3);
+    const height = wrapper.findAll('span').at(5);
     expect(height.text()).toBe('17"');
   });
 
   it('displays the correct color', () => {
-    const color = wrapper.findAll('span').at(5);
+    const color = wrapper.findAll('span').at(7);
     expect(color.text()).toBe('yellow');
     expect(color.attributes('style')).toContain('color: yellow');
   });
 
   it('displays the correct shape', () => {
-    const color = wrapper.findAll('span').at(7);
+    const color = wrapper.findAll('span').at(9);
     expect(color.text()).toBe('upright');
   });
 
   it('displays the correct generation', () => {
-    const color = wrapper.findAll('span').at(9);
+    const color = wrapper.findAll('span').at(11);
     expect(color.text()).toBe('I');
   });
 });
