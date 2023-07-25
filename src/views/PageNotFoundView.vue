da8ad2896ad84ac20f91ca614fdf244eb6a7d033
Nash Aguayo <nash.aguayo@gmail.com>
2023-07-24 22:14:09 -0300

    FEAT: Improved styles of 404 page

--- src/views/PageNotFoundView.vue
+++ src/views/PageNotFoundView.vue
@@ -2,16 +2,34 @@
   <div class="page-not-found-view">
     <h1>404 Error</h1>
     <h2>Page Not Found</h2>
-    <span class="page-not-found-text">
+    <p class="page-not-found-text">
       How did you get here? There's nothing to see!
-    </span>
+    </p>
+    <img
+      :src="silouette"
+      alt="pokemon silouette"
+      :class="{ 'white-silouette': isDarkModeEnabled }"
+    />
   </div>
 </template>
 
 <script>
+import silouette from '@/assets/pokemons/silouette.png';
+import store from '@/lib/store';
+
 export default {
   title: 'Page Not Found',
   name: 'PageNotFoundView',
+  data() {
+    return {
+      silouette,
+    };
+  },
+  computed: {
+    isDarkModeEnabled() {
+      return store.state.isDarkModeEnabled;
+    },
+  },
 };
 </script>
 
@@ -24,6 +42,11 @@ export default {
 
   .page-not-found-text {
     text-align: center;
+    font-family: 'Kanit';
+  }
+
+  .white-silouette {
+    filter: brightness(0) invert(1);
   }
 }
 </style>
