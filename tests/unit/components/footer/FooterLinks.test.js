62edc310d1ce57c41751adc098f8c8f4a856839c
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-02 18:33:22 -0300

    TEST: Fixed failing unit test

--- tests/unit/components/footer/FooterLinks.test.js
+++ tests/unit/components/footer/FooterLinks.test.js
@@ -76,7 +76,9 @@ describe('FooterLinks', () => {
     });
 
     const githubLogo = wrapper.find('.repository img');
-    expect(githubLogo.attributes('src')).toBe('@/assets/ui/github-logo.jpeg');
+    expect(githubLogo.attributes('src')).toBe(
+      '@/assets/images/ui/github-logo.jpeg'
+    );
     expect(githubLogo.attributes('alt')).toBe('github logo');
 
     const githubLogoWrapper = wrapper.find('.repository');
