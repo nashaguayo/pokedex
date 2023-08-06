d9fb0bf1b065e5be5b3b534501be836e9e075f8a
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-05 22:42:00 -0300

    REFACTOR: Extracted random methods

---
+++ src/store/mutations/random.js
@@ -0,0 +1,42 @@
+import random from '@/store/state/random';
+import store from '@/lib/store';
+import { getDataForPokemon as getDataForPokemonApi } from '@/api/pokemon';
+
+export async function getRandomPokemons(amountOfRandomPokemons) {
+  random.clearPokemons();
+  await Promise.all(
+    [...Array(amountOfRandomPokemons)].map(async () => {
+      const pokemon = await getNewRandomPokemon();
+      random.add(pokemon);
+    })
+  );
+}
+
+export async function getNewRandomPokemon(addToRandomPokemon = false) {
+  let newRandomPokemon = {};
+  do {
+    const allPokemons = store.getAllPokemonsReplace();
+    const index = Math.floor(Math.random() * allPokemons.length);
+    const name = allPokemons[index].name;
+    const { image } = await getDataForPokemonApi(name);
+    newRandomPokemon = { name, image };
+  } while (
+    pokemonIsAlreadyInRandomPokemons(newRandomPokemon.name) ||
+    !newRandomPokemon.image
+  );
+
+  if (addToRandomPokemon) {
+    random.popPokemon();
+    random.unshift(newRandomPokemon);
+    return;
+  }
+
+  return newRandomPokemon;
+}
+
+export function pokemonIsAlreadyInRandomPokemons(pokemonName) {
+  const repeatedPokemon = random
+    .getPokemons()
+    .filter((randomPokemon) => randomPokemon.name === pokemonName);
+  return !!repeatedPokemon.length;
+}
