5845568818a4abefc0b020d84b74f80af9877055
Nash Aguayo <nash.aguayo@gmail.com>
2023-08-06 16:32:53 -0300

    REFACTOR: Removed get all pokemons from store

--- src/store/mutations/other.js
+++ src/store/mutations/other.js
@@ -1,9 +1,9 @@
 import other from '@/store/state/other';
-import store from '@/lib/store';
 import {
   isDarkModeEnabled as isDarkModeEnabledInLS,
   toggleDarkMode as toggleDarkModeInLS,
 } from '@/lib/localStorage';
+import { getAll as getAllPokemons } from '@/store/mutations/pokemons';
 import { getAll as getAllGenerations } from '@/store/mutations/generations';
 import { getAll as getAllShapes } from '@/store/mutations/shapes';
 import { getAll as getAllColors } from '@/store/mutations/colors';
@@ -17,7 +17,7 @@ export async function initializeStore() {
       other.toggleIsDarkModeEnabled();
     }
     await Promise.all([
-      store.getAllPokemons(),
+      getAllPokemons(),
       getAllTypes(),
       getAllColors(),
       getAllShapes(),
---
+++ src/store/mutations/pokemons.js
@@ -0,0 +1,45 @@
+import { getAllPokemons as getAllPokemonsApi } from '@/api/pokemon';
+import { setVariant } from '@/store/mutations/variations';
+import pokemonsStore from '@/store/state/pokemons';
+import store from '@/lib/store';
+
+export async function getAll() {
+  const allPokemons = (await getAllPokemonsApi()).results;
+  const allPokemonsWithHyphens = allPokemons.map((pokemon) => ({
+    id: Number(
+      pokemon.url
+        .replace(process.env.VUE_APP_POKEAPI_URL, '')
+        .replace('pokemon/', '')
+        .replace('/', '')
+    ),
+    name: pokemon.name,
+  }));
+
+  const pokemons = [];
+  const variations = [];
+  await Promise.all(
+    allPokemonsWithHyphens.map(async (pokemon) => {
+      if (!pokemon.name.includes('-')) {
+        pokemons.push(pokemon);
+      } else if (await store.pokemonIsVariant(pokemon.name)) {
+        variations.push(pokemon);
+      } else {
+        pokemons.push(pokemon);
+      }
+    })
+  );
+
+  pokemons.forEach((pokemon) => {
+    const variantsForPokemon = variations.filter((v) =>
+      v.name.includes(pokemon.name)
+    );
+    if (variantsForPokemon.length) {
+      setVariant(
+        pokemon.name,
+        variantsForPokemon.map((p) => p.name)
+      );
+    }
+  });
+
+  pokemonsStore.setAll(pokemons);
+}
